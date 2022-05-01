5d24b9842aa3ade63f5cbe2ce55d048e556ff485
viks123456 <92173763+viks123456@users.noreply.github.com>
2022-05-01 22:11:36 +0200

    Update to-do.js

--- to-do-list/to-do.js
+++ to-do-list/to-do.js
@@ -1,10 +1,15 @@
+window.onload = about_btn;
+
 function item_added()
 {
     var new_item = document.createElement("li");
-    var name = document.getElementById("item_name");
+    var text_box = document.getElementById("text_box");
+    var list_ = document.getElementById("list");
 
-    new_item.innerHTML = '<input class="not_completed" type="button" onclick="item_completed(this)" value="'+ name.value + '"/>';
-    document.getElementById("list").appendChild(new_item);
+    new_item.innerHTML = '<input class="not_completed" type="button" onclick="item_completed(this)" value="'+ text_box.value + '"/>';
+    //list_.removeChild(text_box);
+    list_.appendChild(new_item);
+    list_.appendChild(text_box);
 }
 
 function item_completed(item)
@@ -13,4 +18,17 @@ function item_completed(item)
         item.className='completed';
     else 
         item.className='not_completed';
+}
+
+var shown = true;
+
+function about_btn(clear)
+{
+    if(!shown && !clear) {
+        document.getElementById("about_txt").style.display = "block";
+        shown = true;
+    } else if (shown || clear) {
+        document.getElementById("about_txt").style.display = "none";
+        shown = false;
+    }
 }
